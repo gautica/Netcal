8ad875b7067a8ce7f282fc74381e4e355f0b2267
Jan-Christopher Schmidt <y0089528@iz04.ibr.cs.tu-bs.de>
2018-10-30 16:15:39 +0100

    Basic Client Connect

--- client/client.cpp
+++ client/client.cpp
@@ -3,44 +3,18 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
-#include <arpa/inet.h>
-#include <unistd.h>
+#include <netdb.h>
+#define DEFAULT_PORT "5000"
 
-#define PORT 8080
+int main(int argc, char const *argv[]){
+	addrinfo hints;
+	addrinfo* res;
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
 
-int main(int argc, char const *argv[])
-{
-    struct sockaddr_in address;
-    int sock = 0, valread;
-    struct sockaddr_in serv_addr;
-    char *hello = "Hello from client";
-    char buffer[1024] = {0};
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        printf("\n Socket creation error \n");
-        return -1;
-    }
-
-    memset(&serv_addr, '0', sizeof(serv_addr));
-
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
-
-    // Convert IPv4 and IPv6 addresses from text to binary form
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
-    {
-        printf("\nInvalid address/ Address not supported \n");
-        return -1;
-    }
-
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    {
-        printf("\nConnection Failed \n");
-        return -1;
-    }
-    send(sock , hello , strlen(hello) , 0 );
-    printf("Hello message sent\n");
-    valread = read( sock , buffer, 1024);
-    printf("%s\n",buffer );
-    return 0;
+	printf("getaddrinfo: %i\n", getaddrinfo("localhost", argc > 1 ? argv[1] : DEFAULT_PORT, &hints, &res));
+	int s = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+	printf("socket: %i\n", s);
+	printf("connect: %i\n", connect(s, res->ai_addr, res->ai_addrlen));
 }
